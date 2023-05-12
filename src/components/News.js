7329b411c4b5438c9cad581f9e6ceebcfd6b0fd2
Aashish Gulshan <aashishgulshan@gmail.com>
2023-05-12 12:13:03 +0530

    Now we can see the image if iamge not found

--- src/components/News.js
+++ src/components/News.js
@@ -19,7 +19,7 @@ const News = ({cat}) => {
     fetchNews();
     const interval = setInterval(() =>{
      window.location.reload(); 
-    }, 60000);
+    }, 6000);
     return () => clearInterval(interval);
     
     // console.log("Useeffect Called");
