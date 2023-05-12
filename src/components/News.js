9c8f4549eb5879854c66f0e58669717d853aff8a
Aashish Gulshan <aashishgulshan@gmail.com>
2023-05-13 02:06:36 +0530

    API key changed

--- src/components/News.js
+++ src/components/News.js
@@ -9,9 +9,9 @@ const News = ({cat}) => {
   console.log(cat);
   const fetchNews = async () =>{
     await axios.get(
-      cat ? `https://newsapi.org/v2/top-headlines?country=in&category=${cat}&apiKey=d4bcce6bf37d4946b1e6a8c8d4eed362`
+      cat ? `https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=us&category=${cat}&max=10&apikey=ce67cab7c2d95e95f5af5dd1b8826f22`
       
-      : "https://newsapi.org/v2/top-headlines?country=in&apiKey=d4bcce6bf37d4946b1e6a8c8d4eed362"
+      : "https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=us&max=10&apikey=ce67cab7c2d95e95f5af5dd1b8826f22"
     ).then((res)=> setData(res.data.articles));
   };
 
@@ -19,7 +19,7 @@ const News = ({cat}) => {
     fetchNews();
     const interval = setInterval(() =>{
      window.location.reload(); 
-    }, 30000);
+    }, 60000);
     return () => clearInterval(interval);
     
     // console.log("Useeffect Called");
@@ -31,9 +31,9 @@ const News = ({cat}) => {
       {Data ? Data.map((items)=>(
     <>
       <div className='shadow-lg ring-1 ring-gray-300 rounded-lg flex-wrap'>
-              <img className='rounded-t-lg' src={!items.urlToImage 
+              <img className='rounded-t-lg' src={!items.image
                 ? "https://images.hindustantimes.com/img/2022/06/15/1600x900/AFP_9BX2L6_1623489835537_1655275114928.jpg"
-                : items.urlToImage} max-width={360} alt="Not able to load Image"/>
+                : items.image} max-width={360} alt="Not able to load Image"/>
               <h2 className='text-violet-700 p-1 font-semibold font-serif text-xl '>{items.title}</h2>
               <p className=' text-amber-500 ml-1 font-mono'>Published Date:{items.publishedAt.substring(0,10)}</p>
               <a className=' hover:text-sky-600' href={items.url} target='_blank'><p className='p-1'>{items.content}</p></a>
