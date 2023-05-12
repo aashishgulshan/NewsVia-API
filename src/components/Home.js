7df507f0633a25eae7874944d41b555fd712b99a
Aashish Gulshan <aashishgulshan@gmail.com>
2023-05-13 02:14:52 +0530

    API key changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <>
         <div className='text-center bg-sky-500 p-10 border-t-8 border-yellow-400'>
-        <h1 className=' text-violet-700 font-extrabold text-3xl'>NewsVia API</h1>
+        <h1 className=' text-violet-700 font-semibold text-3xl'>NewsVia API</h1>
         <p className='font-semibold font-sans text-amber-300'>Get fastest news and stay upto-dated with NewsVia API</p>
       </div>
     </>
